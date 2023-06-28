32dcdf7db3579cc58130e5e5a9d9e948df288f18
Tuncer Halil <wi22b100@technikum-wien.at>
2023-06-28 18:03:14 +0200

    Initial commit

---
+++ src/main/java/com/example/FactorialCalculator/FactorialCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.FactorialCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactorialCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactorialCalculatorApplication.class, args);
+	}
+
+}
